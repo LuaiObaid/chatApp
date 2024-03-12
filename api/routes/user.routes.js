a53ba3d65ad7d5d025148a28f783ac1f709cde0a
Luai Obaid <Luai.Obaid@knightec.se>
2024-03-12 03:03:49 +0100

    complete chat app

--- api/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-import exppress from "express"
-import protectRoute from "../middleWare/protectRoute.js";
-import { getUsersForSidebar } from "../controllers/user.controller.js";
-
-const router = exppress.Router();
-
-router.get("/", protectRoute,getUsersForSidebar);
-
-export default router
